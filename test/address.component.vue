b023badb0b9bec29a181340b3c0a3780101088e6
shangbin <shangbin@missfresh.cn>
2019-01-30 14:28:45 +0800

    添加Gif说明文件

--- test/address.component.vue
+++ test/address.component.vue
@@ -1,36 +1,37 @@
 <template>
   <div>
     <div class="order-address cell-container" v-if="!addressInfo || !addressInfo.phone_number">
-      <!--<img src="~/assets/create-order/add.png" class="icon-add" @click="chooseAddress" />-->
-      <!--<div class="no-address__text" @click="chooseAddress">-->
-        <!--<div>-->
-          <!--<span class="no-address__text-hi">嘿，我还不知道你的地址！</span>-->
-        <!--</div>-->
-        <!--<div class="no-address__text-go-wrap">-->
-          <!--<span class="no-address__text-go" v-if="shippingAddressList !== 0">请选择配送地址</span>-->
-          <!--<span class="no-address__text-go" v-if="shippingAddressList === 0">请增加配送地址</span>-->
-        <!--</div>-->
-      <!--</div>-->
+      <img src="~/assets/create-order/add.png" class="icon-add" @click="chooseAddress">
+      <div class="no-address__text" @click="chooseAddress">
+        <div>
+          <span class="no-address__text-hi">嘿，我还不知道你的地址！</span>
+        </div>
+        <div class="no-address__text-go-wrap">
+          <span class="no-address__text-go" v-if="shippingAddressList !== 0">请选择配送地址</span>
+          <span class="no-address__text-go" v-if="shippingAddressList === 0">请增加配送地址</span>
+        </div>
+      </div>
       <cell v-if="shippingAddressList !== 0">
         <div class="order-address-content choose-address" @click="chooseAddress">
-          <img src="~assets/common/loc_icon.png" class="icon-loc" />
+          <img src="~assets/common/loc_icon.png" class="icon-loc">
           <div class="address__detail">
             <span class="address__loc" style="font-weight: bold;">请选择配送地址</span>
           </div>
         </div>
       </cell>
       <div v-if="shippingAddressList === 0" class="noAddress" @click="chooseAddress">
-        <img src="~assets/address/icon_address_add.png" class="icon-loc" />
+        <img src="~assets/address/icon_address_add.png" class="icon-loc">
         <span class="text">新增收货地址</span>
       </div>
     </div>
     <div v-else class="order-address cell-container">
       <cell>
         <div class="order-address-content has-address" @click="chooseAddress">
-          <img src="~assets/common/loc_icon.png" class="icon-loc" />
+          <img src="~assets/common/loc_icon.png" class="icon-loc">
           <div class="address__detail">
             <span class="address__user">{{addressInfo.name}}</span>
-            <span class="address__phone">{{hidePhone(addressInfo.phone_number)}}</span><br/>
+            <span class="address__phone">{{hidePhone(addressInfo.phone_number)}}</span>
+            <br>
             <span class="address__loc">{{addressInfo.full_address}}</span>
           </div>
         </div>
@@ -40,33 +41,33 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import cell from './cell.component.vue'
-import { requestAna } from "sdk/common/analytics"
+import { mapState } from "vuex";
+import cell from "./cell.component.vue";
+import { requestAna } from "sdk/common/analytics";
 
 export default {
-  props: ['addressInfo', 'orderForm', 'shippingAddressList'],
+  props: ["addressInfo", "orderForm", "shippingAddressList"],
   components: {
     cell
   },
   methods: {
     chooseAddress() {
-      this.$store.commit('setForm', this.orderForm)
-      requestAna("click_distribute_address", "canteen_order_index")
-      let addressInfo = this.addressInfo
+      this.$store.commit("setForm", this.orderForm);
+      requestAna("click_distribute_address", "canteen_order_index");
+      let addressInfo = this.addressInfo;
       this.$router.push({
-        name: 'addressList',
+        name: "addressList",
         query: {
-          type: 'pick',
-          id: (addressInfo && addressInfo.id) ? addressInfo.id : ''
+          type: "pick",
+          id: addressInfo && addressInfo.id ? addressInfo.id : ""
         }
-      })
+      });
     },
-    hidePhone (phone) {
-      return phone.slice(0, 3) + '****' + phone.slice(7, 11)
+    hidePhone(phone) {
+      return phone.slice(0, 3) + "****" + phone.slice(7, 11);
     }
   }
-}
+};
 </script>
 
 <style scoped>
@@ -74,13 +75,12 @@ export default {
   display: flex;
   justify-content: flex-start;
   align-items: center;
- 
 }
-.choose-address{
+.choose-address {
   height: 50px;
 }
-.has-address{
- height: 78px;
+.has-address {
+  height: 78px;
 }
 
 .cell-container {
@@ -138,24 +138,24 @@ export default {
 .icon-loc {
   width: 14px;
 }
-  /*add*/
-  .noAddress{
-    height: 44px;
-    line-height: 44px;
-    background: #FF4891;
-    border-radius: 2px;
-    padding: 0 15px;
-    margin-top: 10px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-.noAddress img{
+/*add*/
+.noAddress {
+  height: 44px;
+  line-height: 44px;
+  background: #ff4891;
+  border-radius: 2px;
+  padding: 0 15px;
+  margin-top: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.noAddress img {
   /*margin-top: 7px;*/
 }
-  .noAddress .text{
-    font-size: 14px;
-    color: #FFFFFF;
-    margin-left: 10px;
-  }
+.noAddress .text {
+  font-size: 14px;
+  color: #ffffff;
+  margin-left: 10px;
+}
 </style>
